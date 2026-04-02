c822afd30e2d9ba5ffa0bceeebd87de62aa04d50
Alban Xhafa <alban@syntacticops.com>
2026-04-02 11:45:27 +0200

    reowork compact

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import Alpine from "alpinejs";
+import intersect from "@alpinejs/intersect";
+import collapse from "@alpinejs/collapse";
+
+Alpine.plugin(intersect);
+Alpine.plugin(collapse);
 
 window.Alpine = Alpine;
 Alpine.start();
